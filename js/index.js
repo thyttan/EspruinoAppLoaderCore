f1a211f3a4902b44f510e9acf87dce6919def3e4
RKBoss6 <kabironkar@gmail.com>
2026-06-28 22:12:42 -0400

    Add report issue button for each app in app info

--- js/index.js
+++ js/index.js
@@ -330,7 +330,7 @@ function showAppInfo(appid, installedVersion) {
     const infoPart = infoTxt.length>0 ? marked(infoTxt.join("<br>")) : "";
     const changelogPart = changelogText ? changelogText.replace(/\n/g, "<br/>") : "";
     const changeLogHeading = changelogPart ? "<hr><strong>ChangeLog:</strong><br>" : "";
-    showPrompt(app.name + " App Information", infoPart + changeLogHeading + changelogPart, {ok: true,}, false).catch(() => {});
+    showPrompt(app.name + " App Information", infoPart + changeLogHeading + changelogPart, {ok: true,githubIssue:app}, false).catch(() => {});
   });
 }
 function getAppDescription(app) {
